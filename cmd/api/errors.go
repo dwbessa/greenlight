16c6b67103e0f321d45216476505050a7879782c
dwbessa <danielbessa01@gmail.com>
2025-04-21 10:21:22 -0300

    new: custom error responses

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,12 +5,11 @@ import (
 	"net/http"
 )
 
-func (app *application) logError (r *http.Request, err error) {
+func (app *application) logError(r *http.Request, err error) {
 	app.logger.Println(err)
 }
 
-
-func (app *application) errorResponse (w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
 	env := envelope{"error": message}
 
 	err := app.writeJSON(w, status, env, nil)
@@ -35,4 +34,8 @@ func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request)
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
